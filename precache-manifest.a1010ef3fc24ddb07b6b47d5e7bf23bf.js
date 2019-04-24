a3d9747518798227d8fc67368ddcfbe045fc3814
Alexander Litvinovich <litvinovich.alexander@gmail.com>
2019-04-24 10:23:42 +0300

    +

---
+++ precache-manifest.a1010ef3fc24ddb07b6b47d5e7bf23bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b7b192eb76714d8be9",
+    "url": "/static/css/main.47519520.chunk.css"
+  },
+  {
+    "revision": "a3b7b192eb76714d8be9",
+    "url": "/static/js/main.a3b7b192.chunk.js"
+  },
+  {
+    "revision": "341267181b7bc7df7464",
+    "url": "/static/js/1.34126718.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df702bf24441477bbd50ae3fcfba3397",
+    "url": "/index.html"
+  }
+];
